667598b5965edb9d2b5052313c2c5cf3e3f14a46
alessandrokonrad <akonrad6@gmail.com>
2019-04-12 17:43:24 +0200

    BitView project added

--- build/app/intermediates/flutter/release/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: /home/alessandro/Development/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
--- build/app/intermediates/flutter/release/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /home/alessandro/Development/flutter/bin/cache/artifacts/engine/android-arm-release/linux-x64/gen_snapshot
